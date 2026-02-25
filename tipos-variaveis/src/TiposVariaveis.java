ba7c7c9839c9930597d8b352cb78af5b6a316abc
Gustavo Cardoso <cardosogustavo790@gmail.com>
2026-02-24 22:31:27 -0300

    Tipos de dados.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,2 +1,10 @@
 public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 17350000;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salario = 1580;
+    }
 }
