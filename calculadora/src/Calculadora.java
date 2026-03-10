aa13ed6df5d98b87b797fd977ee094e5e5347817
Gustavo Cardoso <cardosogustavo790@gmail.com>
2026-03-09 23:16:19 -0300

    feat: criando classe Comentários e Calculado para aprender sobre comentários e seus tipos

---
+++ calculadora/src/Calculadora.java
@@ -0,0 +1,17 @@
+/**
+* @author Gustavo Cardoso
+* @version 1.0
+* @since 09/03/2026
+**/
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm
+     * @param numeroDois
+     * @return
+     **/
+
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
